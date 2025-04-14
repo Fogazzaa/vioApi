381e49430223365199cd94ad649234e468a203b9
fogazzaa <vfogacacintra@gmail.com>
2025-04-14 16:16:50 -0300

    14-04-25 / atualizando routes e script

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -17,7 +17,7 @@ router.put("/org", orgController.updateOrg);
 router.delete("/org/:id_organizador", orgController.deleteOrg);
 
 router.post("/evento", eventoController.createEvento);
-router.get("/evento", eventoController.getAllEventos);
+router.get("/eventos", eventoController.getAllEventos);
 router.get("/evento/data", eventoController.getEventosPorData);
 router.get("/evento/:data", eventoController.getEventosPorData7Dias);
 router.put("/evento", eventoController.updateEvento);
