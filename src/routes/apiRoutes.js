26e08744299ccad6e1a7929387532c0d5895eee3
fogazzaa <vfogacacintra@gmail.com>
2025-05-14 13:57:43 -0300

    14-05-25 / protegendo todas as rotas

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -8,26 +8,26 @@ const ingController = require("../controllers/ingController");
 
 router.post("/user", userController.createUser);
 router.get("/user", verifyJWT, userController.getAllUsers);
-router.put("/user", userController.updateUser);
-router.delete("/user/:id_usuario", userController.deleteUser);
+router.put("/user", verifyJWT, userController.updateUser);
+router.delete("/user/:id_usuario", verifyJWT, userController.deleteUser);
 router.post("/login", userController.loginUser);
 
-router.post("/org", orgController.createOrg);
-router.get("/org", orgController.getAllOrgs);
-router.put("/org", orgController.updateOrg);
-router.delete("/org/:id_organizador", orgController.deleteOrg);
+router.post("/org", verifyJWT, orgController.createOrg);
+router.get("/org", verifyJWT, orgController.getAllOrgs);
+router.put("/org", verifyJWT, orgController.updateOrg);
+router.delete("/org/:id_organizador", verifyJWT, orgController.deleteOrg);
 
-router.post("/evento", eventoController.createEvento);
-router.get("/eventos", eventoController.getAllEventos);
-router.get("/evento/data", eventoController.getEventosPorData);
-router.get("/evento/:data", eventoController.getEventosPorData7Dias);
-router.put("/evento", eventoController.updateEvento);
-router.delete("/evento/:id_evento", eventoController.deleteEvento);
+router.post("/evento", verifyJWT, eventoController.createEvento);
+router.get("/eventos", verifyJWT, eventoController.getAllEventos);
+router.get("/evento/data", verifyJWT, eventoController.getEventosPorData);
+router.get("/evento/:data", verifyJWT, eventoController.getEventosPorData7Dias);
+router.put("/evento", verifyJWT, eventoController.updateEvento);
+router.delete("/evento/:id_evento", verifyJWT, eventoController.deleteEvento);
 
-router.post("/ing", ingController.createIng);
-router.get("/ing", ingController.getAllIngs);
-router.put("/ing", ingController.updateIng);
-router.delete("/ing/:id_ingresso", ingController.deleteIng);
-router.get("/ing/evento/:id_evento", ingController.getByIdEvento);
+router.post("/ing", verifyJWT, ingController.createIng);
+router.get("/ing", verifyJWT, ingController.getAllIngs);
+router.put("/ing", verifyJWT, ingController.updateIng);
+router.delete("/ing/:id_ingresso", verifyJWT, ingController.deleteIng);
+router.get("/ing/evento/:id_evento", verifyJWT, ingController.getByIdEvento);
 
 module.exports = router;
