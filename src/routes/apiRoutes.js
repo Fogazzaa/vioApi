9742de196c6ca42c50f2526fce3b68f3c3972bd2
fogazza <vfogacacintra@gmail.com>
2025-09-10 09:36:14 -0300

    10-09-25 / refactor: Alterando Rotas

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -22,7 +22,7 @@ router.delete("/org/:id_organizador", verifyJWT, orgController.deleteOrg);
 
 router.post("/evento", upload.single("imagem"), eventoController.createEvento);
 router.get("/evento/imagem/:id", eventoController.getImagemEvento)
-router.get("/eventos", verifyJWT, eventoController.getAllEventos);
+router.get("/eventos", eventoController.getAllEventos);
 router.get("/evento/data", verifyJWT, eventoController.getEventosPorData);
 router.get("/evento/:data", verifyJWT, eventoController.getEventosPorData7Dias);
 router.put("/evento", verifyJWT, eventoController.updateEvento);
